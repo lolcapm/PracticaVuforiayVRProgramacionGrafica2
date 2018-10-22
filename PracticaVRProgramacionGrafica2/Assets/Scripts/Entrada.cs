a4a2c9a05f611d1ecaeac9bd108f1b83bfc78770
lolcapm <amayberc@gmail.com>
2018-10-22 19:11:22 -0400

    Practica de vuforia y VR en la misma escena Mayber Cayoja

---
+++ PracticaVRProgramacionGrafica2/Assets/Scripts/Entrada.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Entrada : MonoBehaviour {
+
+	public void abierto()
+	{
+		SceneManager.LoadScene("VRespacio");
+	}
+}
